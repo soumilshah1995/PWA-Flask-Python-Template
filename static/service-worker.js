ee17254a818926f5b5876d81b56956a645de30ee
Kapilesh Pennichetty <50639146+technocapeman@users.noreply.github.com>
2021-03-21 18:12:03 -0500

    Improved Service Worker to have Offline Functions
    Used https://www.pwabuilder.com/serviceworker 's "Offline copy with Backup offline page" to generate service worker with improved offline functionality.

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,82 +1,54 @@
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
+const CACHE = "pwabuilder-offline-page";
 
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "/static/offline.html";
 
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
 
-const cacheName = 'flask-PWA-v3';
-const filesToCache = [
-    '/',
-    '/static/app.js',
-    '/static/styles.css',
-    '/static/images/pwa-light.png'
-];
-
-
-
-
-
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
 
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
 
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
 
-
-
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-    e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
+        if (preloadResp) {
+          return preloadResp;
         }
-      }));
-    })
-  );
-  return self.clients.claim();
-});
 
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
 
-
-
-
-
-self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request).catch(error => {
-          console.log('Fetch failed; returning offline page instead.', error);
-          let url = e.request.url;
-          let extension = url.split('.').pop();
-
-          if (extension === 'jpg' || extension === 'png') {
-
-              const FALLBACK_IMAGE = `<svg xmlns="http://www.w3.org/2000/svg" width="200" height="180" stroke-linejoin="round">
-                <path stroke="#DDD" stroke-width="25" d="M99,18 15,162H183z"/>
-                <path stroke-width="17" fill="#FFF" d="M99,18 15,162H183z" stroke="#eee"/>
-                <path d="M91,70a9,9 0 0,1 18,0l-5,50a4,4 0 0,1-8,0z" fill="#aaa"/>
-                <circle cy="138" r="9" cx="100" fill="#aaa"/>
-                </svg>`;
-
-              return Promise.resolve(new Response(FALLBACK_IMAGE, {
-                  headers: {
-                      'Content-Type': 'image/svg+xml'
-                  }
-              }));
-          }
-
-          return caches.match('offline.html');
-      });
-    })
-  );
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
